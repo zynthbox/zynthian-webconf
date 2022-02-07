e1ef8299bbef3c25d1704906de25f2a9ddb0eb27
david nelband <dnelband@gmail.com>
2022-02-07 14:51:55 +0100

    webconf file manager

--- metaheader/src/app/file-manager.js
+++ metaheader/src/app/file-manager.js
@@ -10,9 +10,9 @@ const FileManager = () => {
   const fsep = "/";
 
   const [ files, setFiles ] = useState([])
-  // console.log(files,"files")
+  console.log(files,"files")
   const [ displayedFiles, setDisplayedFiles ] = useState([])
-  // console.log(displayedFiles,"displayed files")
+  console.log(displayedFiles,"displayed files")
   const [ folderChain, setFolderChain ] = useState([{ id: 'xcv', name: rootFolder, isDir: true }])
   const [ selectedFolder, setSelectedFolder ] = useState("")
 
