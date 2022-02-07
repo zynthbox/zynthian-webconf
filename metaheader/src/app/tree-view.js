905d244219cb94b063f6c9467497bd308738b5a7
david nelband <dnelband@gmail.com>
2022-02-07 22:58:44 +0100

    webconf file manager tree view added

---
+++ metaheader/src/app/tree-view.js
@@ -0,0 +1,36 @@
+import React, {PureComponent} from 'react';
+import {Treebeard} from 'react-treebeard';
+
+class TreeView extends PureComponent {
+    constructor(props){
+        console.log(props)
+        super(props);
+        this.state = {data:props.data};
+        this.onToggle = this.onToggle.bind(this);
+    }
+    
+    onToggle(node, toggled){
+        const {cursor, data} = this.state;
+        console.log(node.id);
+        if (cursor) {
+            this.setState(() => ({cursor, active: false}));
+        }
+        node.active = true;
+        if (node.children) { 
+            node.toggled = toggled;
+        }
+        this.setState(() => ({cursor: node, data: Object.assign({}, data)}));
+    }
+    
+    render(){
+        const {data} = this.state;
+        return (
+                <Treebeard
+                    data={data}
+                    onToggle={this.onToggle}
+                />
+        );
+    }
+}
+
+export default TreeView
