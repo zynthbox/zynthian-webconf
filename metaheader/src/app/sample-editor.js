9565932b50dcc8469a02c8ce735ab7d1456fc725
david nelband <dnelband@gmail.com>
2022-03-09 13:50:07 +0100

    sample editor progress

--- metaheader/src/app/sample-editor.js
+++ metaheader/src/app/sample-editor.js
@@ -1,32 +1,63 @@
 import React, { useState, useEffect, useRef } from 'react'
-import randomColor from "randomcolor";
+import Dropzone from 'react-dropzone'
 import { SketchPicker } from 'react-color';
 
 const SampleEditor = () => {
+
+    const tracksArray = [
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {}
+    ]
+
+    const colorsArray = [
+        "#B23730",
+        "#EE514B",
+        "#F77535",
+        "#F7D635",
+        "#FE68B1",
+        "#A438FF",
+        "#6491FF",
+        "#73F6EE",
+        "#65E679",
+        "#9A7136",
+    ]
+
+    const [ tracks, setTracks ] = useState(tracksArray)
+
+    const sampleSetsDisplay = colorsArray.map((c,index) => (
+        <Track index={index} color={c}/>
+    ))
+
     return (
         <div id="sample-editor">
-            <SampleSet index={0}/>
-            <SampleSet index={1}/>
-            <SampleSet index={2}/>
-            <SampleSet index={3}/>
-            <SampleSet index={4}/>
-            <SampleSet index={5}/>
-            <SampleSet index={6}/>
-            <SampleSet index={7}/>
-            <SampleSet index={8}/>
-            <SampleSet index={9}/>
+            {sampleSetsDisplay}
         </div>
     )
 }
 
-const SampleSet = (props) => {
+const Track = (props) => {
+
+    const samplesArray = [
+        null,null,null,null,null
+    ]
 
     const { index } = props;
 
+    const [ samples, setSamples ] = useState(samplesArray)
+    console.log(samples,"samples")
     const [ showEditMode, setShowEditMode ] = useState(false);
     const [ showColorPicker, setShowColorPicker ] = useState(false);
-    const [ title, setTitle ] = useState(`Track Title ${index + 1}`);
-    const [ color, setColor ] = useState(randomColor())
+    const [ dragZoneContainerCssClass, setDragZoneContainerCssClass ] = useState('hidden')
+    const [ title, setTitle ] = useState(`Track ${index + 1}`);
+    const [ color, setColor ] = useState(props.color)
 
     const ref = useRef();
     useOnClickOutside(ref, () => setShowColorPicker(false));
@@ -35,22 +66,44 @@ const SampleSet = (props) => {
         setColor(color.hex)
     }
 
-    const samplesArray = [{
-        title:`sample ${index + 1}-1`,
-        url:'URL'
-    },{
-        title:`sample ${index + 1}-2`,
-        url:'URL'
-    },{
-        title:`sample ${index + 1}-3`,
-        url:'URL'
-    },{
-        title:`sample ${index + 1}-4`,
-        url:'URL'
-    },{
-        title:`sample ${index + 1}-5`,
-        url:'URL'
-    }]
+    function onSampleListDragOver(){
+        setDragZoneContainerCssClass('')
+    }
+
+    function onSampleListDragExit(){
+        setDragZoneContainerCssClass('hidden')
+    }
+
+    function onDropSamples(acceptedFiles){
+        setDragZoneContainerCssClass('hidden')
+        acceptedFiles.forEach(function(file,index){
+            addSampleToSampleSet(file);
+            // const reader = new FileReader();
+            // reader.addEventListener("load", function () {
+            //     file.data = reader.result;
+            // }, false);
+            
+            // if (file) {
+            //     reader.readAsDataURL(file);
+            // }
+        })
+
+    }
+
+    function addSampleToSampleSet(file){
+        let sampleIndexToReplace = samples.findIndex((sample) => sample === null);
+        if (!sampleIndexToReplace || sampleIndexToReplace === null) sampleIndexToReplace = 0;
+        const newSamples = [ ...samples.slice(0,sampleIndexToReplace), file, ...samples.slice(sampleIndexToReplace + 1, samples.length)]
+        setSamples(newSamples)
+    }
+
+    function onClearAllClick(){
+        console.log('on clear all click')
+    }
+
+    function onUploadSampleSetClick(){
+        console.log('on upload sample set click')
+    }
 
     let titleDisplay;
     if (showEditMode === true){
@@ -58,7 +111,7 @@ const SampleSet = (props) => {
             <input type="text" value={title} onChange={e => setTitle(e.target.value)}/>
         )
     } else {
-        titleDisplay = title;
+        titleDisplay = <h2>{title}</h2>;
     }
 
     let colorPickerDisplay;
@@ -73,14 +126,21 @@ const SampleSet = (props) => {
         )
     }
 
-    const samplesDisplay = samplesArray.map((sample,i) => (
-        <Sample key={i} sampleSetIndex={index} index={i} sample={sample} />
+    const samplesDisplay = samples.map((sample,i) => (
+        <Sample 
+            key={i} 
+            index={i} 
+            sample={samples[i]} 
+            trackIndex={index} 
+        />
     ))
 
     return (
         <div className="sample-set" style={{backgroundColor:color}}>
             {colorPickerDisplay}
-            <h2>{titleDisplay}</h2>
+            <div className="sample-set-title">
+                {titleDisplay}
+            </div>
             <ul className="edit-menu">
                 <li>
                     <a className="edit-button" onClick={() => setShowEditMode(showEditMode == true ? false : true)}>
@@ -94,8 +154,26 @@ const SampleSet = (props) => {
                     </a>
                 </li>
             </ul>
-            <ul className="sample-list">
-                {samplesDisplay}
+            <div className={"sample-list-container"} onDragOver={onSampleListDragOver} onDragLeave={onSampleListDragExit}>
+                <div className={"dropzone-container " + dragZoneContainerCssClass}>
+                    <Dropzone onDrop={acceptedFiles => onDropSamples(acceptedFiles)}>
+                        {({getRootProps, getInputProps}) => (
+                            <section>
+                            <div {...getRootProps()}>
+                                <input {...getInputProps()} />
+                                <p>Drag 'n' drop some files here</p>
+                            </div>
+                            </section>
+                        )}
+                    </Dropzone>    
+                </div>
+                <ul className="sample-list">
+                    {samplesDisplay}
+                </ul>
+            </div>
+            <ul className="sample-set-actions">
+                <li><a onClick={() => onClearAllClick()}><i className="glyphicon glyphicon-trash"></i> Clear All</a></li>
+                <li><a onClick={() => onUploadSampleSetClick()}><i className="glyphicon glyphicon-plus"></i> Upload Sample set</a></li>
             </ul>
         </div>
     )
@@ -103,12 +181,41 @@ const SampleSet = (props) => {
 
 const Sample = (props) => {
 
-    const { index, sample, sampleSetIndex } = props
+    const { index, sample, trackIndex } = props
+
+    console.log(sample)
+
+    function onRemoveSampleClick(){
+        console.log('remove sample')
+    }
+
+    function onAddSampleClick(){
+        console.log('add sample')
+    }
+
+    let sampleActionsDisplay;
+    if (sample){
+        sampleActionsDisplay = (
+            <a className="edit-sample-button" onClick={() => onRemoveSampleClick()}>
+                <i className="glyphicon glyphicon-trash"></i> 
+                Remove
+            </a>
+                
+        )
+    } else {
+        sampleActionsDisplay = (
+            <a className="edit-sample-button" onClick={() => onAddSampleClick()}>
+                <i className="glyphicon glyphicon-plus"></i> 
+                Add
+            </a>            
+        )
+    }
 
     return (
-        <li id={`sample-${sampleSetIndex + 1}-${index + 1}`}>
+        <li id={`sample-${trackIndex + 1}-${index + 1}`}>
             <i className='glyphicon glyphicon-play-circle'></i>
-            <h4>{sample.title}</h4>
+            <h4>{sample ? sample.name : '---'}</h4>
+            {sampleActionsDisplay}
         </li>
     )
 }
@@ -139,6 +246,6 @@ function useOnClickOutside(ref, handler) {
       // ... passing it into this hook.
       [ref, handler]
     );
-  }
+}
 
 export default SampleEditor
