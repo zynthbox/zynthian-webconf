6b6896d8dd4c662e805e9099ca39ec4e145432a9
david nelband <dnelband@gmail.com>
2022-04-01 15:59:38 +0200

    metaheader - sketchPad Xtractor initial version

---
+++ metaheader/src/app/sketchpad-xtractor/sketchpad-xtractor.js
@@ -0,0 +1,228 @@
+import React, { useState, useEffect } from 'react';
+
+function SketchPadXtractor(props){
+
+    const { colorsArray } = props
+
+    const initSketchFolder = {path:"sketches/my-sketches/temp/"}
+
+    const [ folders, setFolders ] = useState([initSketchFolder])
+    const [ selectedSketchFolder, setSelectedSketchFolder ] = useState(null)
+    
+    const [ sketchVersions, setSketchVersions ] = useState(null)
+    const [ selectedSketchVersion, setSelectedSketchVersion ] = useState(null)
+
+    const [ isGeneratingItemGroups, setIsGeneratingItemGroups ] = useState(false);
+    const [ itemGroupsGenerationIndex, setItemGroupsGenerationIndex ] = useState(0);
+
+    const [ sketchItemGroups, setSketchItemGroups ] = useState(null)
+    console.log(sketchItemGroups,"sketch item groups")
+    const [ selectedSketchItemGroup, setSelectedSketchItemGroup ] = useState(null)
+
+    const [ sketchItemGroupItems, setSketchItemGroupItems ] = useState(null)
+    const [ selectedSketchItemGroupItem, setSelectedSketchItemGroupItem ] = useState(null)
+
+    useEffect(() => {
+        if (selectedSketchFolder !== null){
+            getSketchVersions()
+        }
+    },[selectedSketchFolder])
+
+    useEffect(() => {
+        if (selectedSketchVersion !== null){
+            getSketch()
+        }
+    },[selectedSketchVersion])
+
+    async function getSketchVersions(){
+
+        const folderPath = selectedSketchFolder.path.split('/').join('+++');
+        const response = await fetch(`http://${window.location.hostname}:3000/mydata/${folderPath}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const res = await response.json();
+        
+        let newSketchVersions = [];
+
+        res.forEach(function(sv,index){
+            if (sv.path.indexOf(selectedSketchFolder.path) > -1){
+                if (sv.path.indexOf("sketch.json") > -1) newSketchVersions.push(sv)
+            }
+        })
+
+        setSketchVersions(newSketchVersions)
+    }
+
+    async function getSketch(){
+        const response = await fetch(`http://${window.location.hostname}:3000/sketch/${selectedSketchVersion.path.split('/').join('+++')}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        const sketchJson = await response.json();
+        console.log(sketchJson,"get sketch res");
+        generateItemGroups(sketchJson)
+        // setCurrentSketch(res)    
+    }
+
+    async function generateItemGroups(sketchJson){
+
+        const newItemGroups = {
+            clips:[],
+            samples:[],
+            sounds:[],
+            patterns:[]
+        }
+        const letters = ["a","b","c","d","e","f","g","h","i","j"]
+        const emptyPatternNotesLength = 25283;
+
+        sketchJson.tracks.forEach(async function(track,index){
+
+            // if (index + 1 === sketchJson.tracks.length) setSketchItemGroups(newItemGroups)
+
+            track.clips.forEach(async function(clip,cIndex){
+                
+                // clips
+                if (clip.path !== null) newItemGroups.clips.push(clip)
+
+                // patterns
+
+                const patternPath = `/home/pi/zynthian-my-data/${selectedSketchFolder.path}sequences/scene-${letters[index]}/patterns/scene-${letters[index]}-${cIndex}.pattern.json`
+
+                const patternResponse = await fetch(`http://${window.location.hostname}:3000/json/${patternPath.split('/').join('+++')}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                });
+                const pattern = await patternResponse.json();
+                if (pattern.notes.length !== emptyPatternNotesLength){
+                    newItemGroups.patterns.push(pattern)
+                }
+            })
+
+            // samples
+
+            const samplesResponse = await fetch(`http://${window.location.hostname}:3000/track/${index+1}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            const samples = await samplesResponse.json();
+            
+            samples.forEach(function(sample,sIndex){
+                if (sample !== null) newItemGroups.samples.push(sample)
+            })
+
+            // sounds
+            console.log(index + 1,sketchJson.tracks.length)
+            setSketchItemGroups(newItemGroups)
+        })
+    }
+
+    let sketchVersionColumnDisplay;
+    if (sketchVersions !== null){
+        sketchVersionColumnDisplay = (
+            <SketchPadXtractorColumn 
+                type="versions"
+                items={sketchVersions}
+                onSelectItem={setSelectedSketchVersion}
+                color={colorsArray[1]}
+            />            
+        )
+    }
+
+    let sketchItemGroupColumnDisplay;
+    if (sketchItemGroups !== null){
+        sketchItemGroupColumnDisplay = (
+            <SketchPadXtractorColumn 
+                type={"item groups"}
+                items={sketchItemGroups}
+                onSelectItem={setSketchItemGroupItems}
+                color={colorsArray[2]}
+             />
+        )
+    }
+
+    let sketchItemGroupItemsColumnDisplay;
+    if (sketchItemGroupItems !== null){
+        sketchItemGroupItemsColumnDisplay = (
+            <SketchPadXtractorColumn 
+                type={"items"}
+                items={sketchItemGroupItems}
+                onSelectItem={setSelectedSketchItemGroupItem}
+                color={colorsArray[3]}
+             />
+        )
+    }
+
+    return (
+        <div id="sketch-pad-xtractor">
+            <div className='sketch-pad-xtractor-column' style={{backgroundColor:colorsArray[0]}}>
+                <h4>Folders</h4>
+                <SketchPadXtractorColumn 
+                    type="folders"
+                    items={folders}
+                    onSelectItem={setSelectedSketchFolder}
+                    color={colorsArray[0]}
+                />
+            </div>
+            <div className='sketch-pad-xtractor-column' style={{backgroundColor:colorsArray[1]}}>
+                <h4>Versions</h4>
+                {sketchVersionColumnDisplay}
+            </div>
+            <div className='sketch-pad-xtractor-column' style={{backgroundColor:colorsArray[2]}}>
+                <h4>Item Groups</h4>
+                {sketchItemGroupColumnDisplay}
+            </div>
+            <div className='sketch-pad-xtractor-column' style={{backgroundColor:colorsArray[3]}}>
+                <h4>Items</h4>
+                {sketchItemGroupItemsColumnDisplay}
+            </div>
+            <div className='sketch-pad-xtractor-column' style={{backgroundColor:colorsArray[4]}}>
+                
+            </div>
+        </div>
+    )
+}
+
+function SketchPadXtractorColumn(props){
+
+    const { type, items, onSelectItem, color } = props
+
+    let itemsDisplay;
+    if (type === "item groups"){
+        itemsDisplay = (
+            <React.Fragment>
+                <li><a onClick={() => onSelectItem(items.clips,"clips")}>Clips ({items.clips !== null ? items.clips.length: 0})</a></li>
+                <li><a onClick={() => onSelectItem(items.samples,"samples")}>Samples ({items.samples !== null ? items.samples.length: 0})</a></li>
+                <li><a onClick={() => onSelectItem(items.patterns,"patterns")}>Patterns ({items.patterns !== null ? items.patterns.length: 0})</a></li>
+                <li><a onClick={() => onSelectItem(items.sounds,"sounds")}>Sounds ({items.sounds !== null ? items.sounds.length: 0})</a></li>
+            </React.Fragment>
+        )
+    } else {
+        itemsDisplay = items.map((item,index) => (
+            <li key={index}>
+                <a onClick={() => onSelectItem(item)}>{item.path ? item.path : `item ${index + 1}`}</a>
+            </li>
+        ))
+    }
+
+    return (
+        <React.Fragment>
+            <ul>
+                {itemsDisplay}
+            </ul>
+        </React.Fragment>
+    )
+}
+
+
+
+export default SketchPadXtractor;
--- metaheader/src/index.js
+++ metaheader/src/index.js
@@ -1,21 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
+
+import { setChonkyDefaults } from 'chonky';
+import { ChonkyIconFA } from 'chonky-icon-fontawesome';
+
 import FileManager from './app/file-manager/file-manager';
 import SampleEditor from './app/sample-editor/sample-editor'
 import Favorites from './app/favorites/favorites';
-import { setChonkyDefaults } from 'chonky';
-import { ChonkyIconFA } from 'chonky-icon-fontawesome';
+import SketchPadXtractor from './app/sketchpad-xtractor/sketchpad-xtractor';
 
 function MetaHeader(){
 
     const [ showFileManager, setShowFileManager ] = useState(false)
     const [ showSampleEditor, setShowSampleEditor ] = useState(false);
     const [ showFavorites, setShowFavorites ] = useState(false)
+    const [ showXtractor, setShowXtractor ] = useState(false)
 
     useEffect(() => {
         if (showFileManager === true){
             setShowSampleEditor(false)
             setShowFavorites(false)
+            setShowXtractor(false)
         }
     },[showFileManager])
 
@@ -23,6 +28,7 @@ function MetaHeader(){
         if (showSampleEditor === true){
             setShowFileManager(false)
             setShowFavorites(false)
+            setShowXtractor(false)
         }
     },[showSampleEditor])
 
@@ -30,8 +36,30 @@ function MetaHeader(){
         if (showFavorites === true){
             setShowFileManager(false)
             setShowSampleEditor(false)
+            setShowXtractor(false)
         }
     },[showFavorites])
+
+    useEffect(() => {
+        if (showXtractor === true){
+            setShowFileManager(false)
+            setShowSampleEditor(false)
+            setShowFavorites(false)
+        }
+    },[showXtractor])
+
+    const colorsArray = [
+        "#B23730",
+        "#EE514B",
+        "#F77535",
+        "#F7D635",
+        "#FE68B1",
+        "#A438FF",
+        "#6491FF",
+        "#73F6EE",
+        "#65E679",
+        "#9A7136",
+    ]
 
     let fileManagerDisplay;
     let fileManagerLeftCss = 0;
@@ -55,19 +83,6 @@ function MetaHeader(){
             </React.Fragment>
         )
     }
-
-    const colorsArray = [
-        "#B23730",
-        "#EE514B",
-        "#F77535",
-        "#F7D635",
-        "#FE68B1",
-        "#A438FF",
-        "#6491FF",
-        "#73F6EE",
-        "#65E679",
-        "#9A7136",
-    ]
 
     let sampleEditorDisplay;
     if (showSampleEditor === true){
@@ -99,6 +114,21 @@ function MetaHeader(){
         )
     }
 
+    let xtractorDisplay;
+    if (showXtractor === true){
+        xtractorDisplay = (
+            <React.Fragment>
+                <div id="sketch-pad-xtractor-container" className="container" style={{left:fileManagerLeftCss}}>
+                    <h3>
+                        SketchPad Xtractor
+                    </h3>
+                    <SketchPadXtractor colorsArray={colorsArray} />
+                </div>
+                <div id="sketch-pad-xtractor-overlay"></div>
+            </React.Fragment>
+        )
+    }
+
     return (
         <React.Fragment>
             <a href="#m" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">More <span className="caret"></span></a>
@@ -112,10 +142,14 @@ function MetaHeader(){
                 <li>
                     <a className={showFavorites === true ? "active" : ""} style={{cursor:"pointer"}} onClick={() => setShowFavorites(showFavorites === true ? false : true)}>Favorites</a>
                 </li>
+                <li>
+                    <a className={showXtractor === true ? "active" : ""} style={{cursor:"pointer"}} onClick={() => setShowXtractor(showXtractor === true ? false : true)}>SketchPad Xtractor</a>
+                </li>
             </ul>
             {fileManagerDisplay}
             {sampleEditorDisplay}
             {favoritesDisplay}
+            {xtractorDisplay}
         </React.Fragment>
     )
 }
