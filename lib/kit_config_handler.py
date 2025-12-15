ab3148c5a018d10bd56c9ed350e215d79c007323
Anupam Basak <anupam.basak27@gmail.com>
2025-12-15 18:32:55 +0530

    kit_config : Handle custom kit

--- lib/kit_config_handler.py
+++ lib/kit_config_handler.py
@@ -26,9 +26,10 @@ import os
 import re
 import logging
 import tornado.web
+import subprocess
 from collections import OrderedDict
-from subprocess import check_output, call
 from pathlib import Path
+from datetime import datetime
 
 from lib.zynthian_config_handler import ZynthianConfigHandler
 from lib.audio_config_handler import soundcard_presets
@@ -87,36 +88,55 @@ class KitConfigHandler(ZynthianConfigHandler):
 
 
     def configure_kit(self, pconfig):
+        errors = None
         kit_version = pconfig['ZYNTHIAN_KIT_VERSION'][0]
-        if kit_version == "AUTO_DETECT":
-            # If auto detect is selected, delete forced_kit_version_file
-            self.forced_kit_version_file.unlink(missing_ok=True)
+
+        # Reset forced kit version. This file will be written at the end of this method
+        self.forced_kit_version_file.unlink(missing_ok=True)
+
+        # If Custom kit is selected, populate users env with the current kit defaults
+        if kit_version == "Custom":
+            # Fetch soundcard name and display name from current kit's default envars file
+            detected_kit = subprocess.run(['python', '/zynthian/zynthian-sys/sbin/detect_zynthbox_kit.py'], capture_output=True, text=True, check=True).stdout.strip()
+            kit_envars_file = Path(f"/zynthian/zynthian-sys/config/envars/zynthian_envars_{detected_kit}.sh")
+            if kit_envars_file.exists():
+                data = kit_envars_file.read_text()
+                soundcard_match = re.search(r'export SOUNDCARD_NAME="(.+)"', data)
+                display_match = re.search(r'export DISPLAY_NAME="(.+)"', data)
+
+                # Write soundcard and display config to user env
+                if soundcard_match is not None and display_match is not None:
+                    soundcard_name = soundcard_match.group(1)
+                    display_name = display_match.group(1)
+
+                    if soundcard_name in soundcard_presets:
+                        pconfig['SOUNDCARD_NAME']=[soundcard_name]
+                        for k,v in soundcard_presets[soundcard_name].items():
+                            pconfig[k]=[v]
+                    else:
+                        logging.error(f"Soundcard {soundcard_name} not found")
+
+                    if display_name in DisplayConfigHandler.display_presets:
+                        pconfig['DISPLAY_NAME']=[display_name]
+                        for k,v in DisplayConfigHandler.display_presets[display_name].items():
+                            pconfig[k]=[v]
+                    else:
+                        logging.error(f"Display {display_name} not found")
+
+                    errors = self.update_config(pconfig)
         else:
-            # Otherwise write forced kit to forced_kit_version_file
+            # If any other kit is selected, back up existing users env and remove to reset overridden values
+            user_envars_file = Path("/zynthian/config/zynthian_envars.user.sh")
+            if user_envars_file.exists() and len(user_envars_file.read_text().strip()) > 0:
+                # Back up user envars file if it exists and has some content
+                user_envars_file.rename(f"/zynthian/config/zynthian_envars_{datetime.now().strftime('%Y%m%dT%H%M%S')}.user.sh")
+
+        if not kit_version == "AUTO_DETECT":
+            # Write forced kit to forced_kit_version_file if kit version is not set to AUTO_DETECT
             with open(self.forced_kit_version_file, "w") as f:
                 f.write(kit_version)
 
-            # if kit_version in ("Z2_V4"):
-            #     soundcard_name = "Z2 ADAC"
-            #     display_name = "Z2 Display"
-            #     wiring_layout = "Z2_V4"
-            # elif kit_version in ("Z2_V5"):
-            #     soundcard_name = "Z2 ADAC"
-            #     display_name = "Waveshare 1280x800 LCD DSI"
-            #     wiring_layout = "Z2_V5"
-            #
-            # pconfig['SOUNDCARD_NAME']=[soundcard_name]
-            # for k,v in soundcard_presets[soundcard_name].items():
-            #     pconfig[k]=[v]
-            #
-            # pconfig['DISPLAY_NAME']=[display_name]
-            # for k,v in DisplayConfigHandler.display_presets[display_name].items():
-            #     pconfig[k]=[v]
-            #
-            # pconfig['ZYNTHIAN_WIRING_LAYOUT']=[wiring_layout]
-            # for k,v in WiringConfigHandler.wiring_presets[wiring_layout].items():
-            #     pconfig[k]=[v]
+        # Set selected kit version to envars
+        os.environ["ZYNTHIAN_KIT_VERSION"] = kit_version
 
-        # errors = self.update_config(pconfig)
-        # DisplayConfigHandler.delete_fb_splash()
-        # WiringConfigHandler.rebuild_zyncoder()
+        return errors
